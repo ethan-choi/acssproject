b04dcaf4f8c008e13148dfb5e1ed4fdad504899f
eethanchoi <ethanchoi777@gmail.com>
2021-03-24 20:30:05 +1100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -14,6 +14,7 @@ import oday1 from "./img/oday/oday1.JPG"
 function About() {
   return (
     <div className="about">
+      {/* This is a test */}
       {/* Navigation bar */}
       <Navigation> </Navigation>
 
@@ -95,7 +96,7 @@ function About() {
                   <p> Love food? We'll take you to some of the best places to eat and drink! </p>
                 </div>
               </Col>
-              <Col lg={{ span: 4 }}> 
+              <Col lg={{ span: 4 }}>
                 <div class="">
                   <Image className="benefitsImage" src={oday7} fluid />
                 </div>
@@ -124,11 +125,11 @@ function About() {
       {/* Ready to Join */}
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffa800" fill-opacity="1" d="M0,96L34.3,90.7C68.6,85,137,75,206,58.7C274.3,43,343,21,411,16C480,11,549,21,617,48C685.7,75,754,117,823,133.3C891.4,149,960,139,1029,133.3C1097.1,128,1166,128,1234,128C1302.9,128,1371,128,1406,128L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
       <div class="readyContainer">
-      <Row>
-        <Col>
-          <h1 class="readyText">Ready to join?</h1>
-        </Col>
-       </Row>
+        <Row>
+          <Col>
+            <h1 class="readyText">Ready to join?</h1>
+          </Col>
+        </Row>
         <Container fluid={true}>
           <Row>
             <Col lg={{ span: 2, offset: 5 }}>
